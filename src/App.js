94d7e940dc871d804275439a6bd1c0a18b5ea70f
Richard Rosa-Serrano <richardjrosaserrano@gmail.com>
2022-02-01 18:10:06 -0500

    Create the function to delete the card when clicked on

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
+    
     this.state= {
       ideas: [
         { id: 1, title:  'Prank Travis', description: 'Stick googly eyes on all his stuff'},
@@ -19,13 +20,19 @@ class App extends Component {
     this.setState({ideas: [...this.state.ideas, newIdea]})
   }
 
+  deleteIdea = (id) => {
+    console.log(id);
+    const filteredIdeas = this.state.ideas.filter(idea => idea.id != id)
+    this.setState({ ideas: filteredIdeas })
+  }
+
   render() {
     return (
       <main className='App'>
       <h1>IdeaBox</h1>
       {!this.state.ideas.length && <h2>No ideas yet -- add some!</h2>}
       <Form addIdea={this.addIdea} />
-      <Ideas ideas={this.state.ideas} />
+      <Ideas ideas={this.state.ideas} deleteIdea={this.deleteIdea} />
       </main>
     )
   }
